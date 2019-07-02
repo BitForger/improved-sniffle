c30db4a0bfe32350b93e3896c6247a24835a2d64
Noah Kovacs <nkovacs@weare5stones.com>
2019-07-02 09:41:02 -0500

    feat(): add basics for hammerspoon messaging

--- init.lua
+++ init.lua
@@ -1,23 +1,29 @@
-hs.loadSpoon('RoundedCorners')
+--hs.loadSpoon('RoundedCorners')
 
-spoon.RoundedCorners:start();
+--spoon.RoundedCorners:start();
+
+local logger = hs.logger.new('Chooser', 'debug')
 
 function sleep(n)
   os.execute("sleep " .. tonumber(n))
 end
 
 hs.hotkey.bind({"cmd", "alt"}, "s", function()
-	for i = 0,50 do
-		hs.spotify.setVolume(100 - i);
-		sleep(.01)
+	if hs.spotify.getVolume() >= 100 then
+		for i = 0,50 do
+			hs.spotify.setVolume(100 - i);
+			sleep(.01)
+		end
 	end
 end)
 
 
 hs.hotkey.bind({"cmd", "alt", "shift"}, "s", function()
-	for i = 0,50 do
-		hs.spotify.setVolume(50 + i);
-		sleep(.01)
+	if hs.spotify.getVolume() <= 50 then
+		for i = 0,50 do
+			hs.spotify.setVolume(50 + i);
+			sleep(.01)
+		end
 	end
 end)
 
@@ -25,6 +31,27 @@ hs.hotkey.bind({"cmd", "alt"}, "m", function()
 	hs.spotify.displayCurrentTrack()
 end)
 
-hs.hotkey.bind({"fn"}, "F12", function ()
+hs.hotkey.bind({"alt"}, "F12", function ()
 	hs.spotify.playpause()
+end)
+
+hs.hotkey.bind({"shift", "cmd"}, "u", function ()
+	local choices = {
+		{
+			["text"] = "First",
+			["subText"] = "This is subtext",
+			["uuid"] = hs.host.uuid()
+		},
+		{
+			["text"] = "Second",
+			["subText"] = "This is subtext",
+			["uuid"] = hs.host.uuid()
+		}
+	}
+	local chooser = hs.chooser.new(function (item)
+		logger.i(hs.json.encode(item))
+	end)
+	chooser:choices(choices)
+	
+	chooser:show()
 end)
